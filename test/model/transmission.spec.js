a830f4ab9484ff09209854c0e8b4575f4bfbf0b2
Allen Gilliland <agilliland@gmail.com>
2017-03-06 15:59:59 -0800

    update to import reference.

--- test/model/transmission.spec.js
+++ test/model/transmission.spec.js
@@ -3,13 +3,13 @@ import {
 } from "model/transmission";
 import {MathHelper} from "util/math";
 
-import {buildingElements} from "testdata/epc";
+import {buildingElements, heatTransferCoeffiecient} from "testdata/epc";
 
 describe("Transmission Heat Transfer", function() {
     it("can calculate heat transfer coefficient", function() {
         let walls = buildingElements.filter((elem) => elem.type === "wall" || elem.type === "roof");
         let windows = buildingElements.filter((elem) => elem.type === "window");
 
-        expect(MathHelper.round(heatTransferByTransmissionCoefficient(walls, windows), 2)).toBe(414.76);
+        expect(MathHelper.round(heatTransferByTransmissionCoefficient(walls, windows), 5)).toBe(heatTransferCoeffiecient);
     });
 });
