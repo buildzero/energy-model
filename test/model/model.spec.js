fab16b799b56cb56be3e9a6f7250bc1f09337bb4
Allen Gilliland <agilliland@gmail.com>
2017-03-22 12:28:12 -0700

    tidy up unit testing numeric precision comparators.

--- test/model/model.spec.js
+++ test/model/model.spec.js
@@ -1,3 +1,5 @@
+import _ from "underscore";
+
 import {
     heatTransferTransmission,
     heatTransferVentilationCoefficient,
@@ -5,8 +7,6 @@ import {
     heatGainSolar,
     indoorConditions
 } from "model/model";
-
-import {MathHelper} from "util/math";
 
 import {
     buildingSettings,
@@ -25,30 +25,82 @@ import {
 } from "testdata/epc";
 
 
+function toHaveCloseValuesTo(actual, expected, precision) {
+    if (precision !== 0) {
+        precision = precision || 2;
+    }
+
+    if (Array.isArray(expected)) {
+        if (!Array.isArray(actual)) {
+            return {pass: false, message: "Expected an array but got a '"+typeof(actual)+"'"};
+        } else if (expected.length !== actual.length) {
+            return {pass: false, message: "Expected array of length "+expected.length+" but actual array was of length "+actual.length};
+        } else {
+            // recur for each array index
+            let results = expected.map((v, i) => toHaveCloseValuesTo(expected[i], actual[i], precision));
+            let error = _.find(results, (r) => r.pass === false);
+            
+            if (error) {
+                return error;
+            } else {
+                return {pass: true};
+            }
+        }
+
+    } else if (typeof(expected) === "object") {
+        // recur for each key
+        let results = Object.keys(expected).map((k) => toHaveCloseValuesTo(expected[k], actual[k], precision));
+        let error = _.find(results, (r) => r.pass === false);
+
+        if (error) {
+            return error;
+        } else {
+            return {pass: true};
+        }
+
+    } else if (_.isNumber(expected)) {
+        return {
+            pass: Math.abs(expected - actual) < (Math.pow(10, -precision) / 2)
+        };
+
+    } else if (_.isString(expected)) {
+        return {
+            pass: expected === actual
+        };
+
+    } else {
+        return {
+            pass: false,
+            message: "Expected value type of '"+typeof(expected)+"' is not an object, array, or number.  Use a different comparitor!"
+        }
+    }
+}
+
+beforeEach(function(){
+    jasmine.addMatchers({
+        toHaveCloseValuesTo: function(util, customEqualityTesters) {
+            return {
+                compare: toHaveCloseValuesTo
+            };
+        }
+    });
+});
+
 
 describe("Transmission Heat Transfer", function() {
     for (let mon = 0; mon < 12; mon++) {
         it("can calculate transmission HT, month="+mon, function() {
-            expect(MathHelper.roundValues(heatTransferTransmission(mon+1, averageIndoorConditions.heating[mon], averageIndoorConditions.cooling[mon], climate[mon], heatTransferCoeffiecient), 4)).toEqual(transmissionHeatTransfer[mon]);
+            let transmissionHT = heatTransferTransmission(mon+1, averageIndoorConditions.heating[mon], averageIndoorConditions.cooling[mon], climate[mon], heatTransferCoeffiecient);
+            expect(transmissionHT).toHaveCloseValuesTo(transmissionHeatTransfer[mon], 3);
         });
     } 
 });
 
 describe("Ventilation Heat Transfer", function() {
-    // TODO: re-enable this when we get the values lined up
-    // it("can calculate ventilation HT coefficient monthly", function() {
-    //     for (var mon = 0; mon < 12; mon++) {
-    //         expect(MathHelper.roundValues(heatTransferVentilationCoefficient(mon+1, buildingSettings, hourlyConditions, climate[mon]), 4)).toEqual(ventilationCoefficientDetailed[mon]);
-    //     }
-    // });
-
     for (let mon = 0; mon < 12; mon++) {
         it("can calculate ventilation coefficient, month="+mon, function() {
-        
             let coeffs = heatTransferVentilationCoefficient(mon+1, buildingSettings, hourlyConditions, climate[mon]);
-
-            expect(MathHelper.round(coeffs.heating.average, 4)).toEqual(ventilationCoefficientDetailed[mon].heating.average);
-            expect(MathHelper.round(coeffs.cooling.average, 4)).toEqual(ventilationCoefficientDetailed[mon].cooling.average);
+            expect(coeffs).toHaveCloseValuesTo(ventilationCoefficientDetailed[mon], 3);
         });
     }
 });
@@ -58,7 +110,8 @@ describe("Internal Gains", function() {
     for (let mon = 0; mon < 12; mon++) {
         it("can calculate internal gains, month="+mon, function() {
             // TODO: better decimal precision
-            expect(MathHelper.roundValues(heatGainInternal(mon+1, hourlyConditions, buildingSettings), 0)).toEqual(internalGains[mon]);
+            let gains = heatGainInternal(mon+1, hourlyConditions, buildingSettings);
+            expect(gains).toHaveCloseValuesTo(internalGains[mon], 0);
         });
     }
 });
@@ -67,14 +120,15 @@ describe("Internal Gains", function() {
 describe("Solar Gains", function() {
     for (let mon = 0; mon < 12; mon++) {
         it("can calculate solar gains, month="+mon, function() {
-            // TODO: ideally these would match at even more granular precision
-            expect(MathHelper.roundValues(heatGainSolar(mon+1, climate[mon], buildingElements), 2)).toEqual(MathHelper.roundValues(solarGains[mon], 2));
+            let gains = heatGainSolar(mon+1, climate[mon], buildingElements);
+            expect(gains).toHaveCloseValuesTo(solarGains[mon], 3);
         });
     }
 });
 
 describe("Indoor Conditions", function() {
     it("can calculate indoor set point conditions", function() {
-        expect(MathHelper.roundValues(indoorConditions(buildingSettings, hourlyConditions, heatTransferCoeffiecient, totalGainsDetailed, ventilationCoefficientDetailed, climate), 3)).toEqual(MathHelper.roundValues(averageIndoorConditions, 3));
+        let conditions = indoorConditions(buildingSettings, hourlyConditions, heatTransferCoeffiecient, totalGainsDetailed, ventilationCoefficientDetailed, climate);
+        expect(conditions).toHaveCloseValuesTo(averageIndoorConditions, 3);
     });
 });
--- test/run.js
+++ test/run.js
@@ -1,5 +1,5 @@
 import Jasmine from 'jasmine'
 
-var jasmine = new Jasmine()
-jasmine.loadConfigFile('jasmine.json')
-jasmine.execute()
+let jasmine = new Jasmine()
+jasmine.loadConfigFile('jasmine.json');
+jasmine.execute();
